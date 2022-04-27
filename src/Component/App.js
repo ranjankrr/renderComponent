b75bd89d830b5893afdc5e5d28e09c6d1b495e98
pannelal <ranjankrr7892@gmail.com>
2022-04-27 17:17:30 +0530

    upload

---
+++ src/Component/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div id="main">
+           <h1 data-ns-test="project-name">ToDOList</h1>
+           <p data-ns-test="project-description">A create todo list using react js</p>
+    </div>
+  )
+}
+
+
+export default App;
